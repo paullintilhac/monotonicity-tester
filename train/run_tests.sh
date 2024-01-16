1e0dccd69f15cc34db48f1b5d0b413d5aa122028
Paul S. Lintilhac <d28979q@g10.hpcc.dartmouth.edu>
2024-01-16 12:38:47 -0500

    update run_tests

--- train/run_tests.sh
+++ train/run_tests.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
  
 
-SBATCH --account=temfom0  # Specify the account to charge
+#SBATCH --account=temfom0  # Specify the account to charge
 
-SBATCH --job-name=train_models_and_test_monotonicity  # Job name
+#SBATCH --job-name=train_models_and_test_monotonicity  # Job name
 
-SBATCH --output=my_job_%j.out  # Standard output and error log
+#SBATCH --output=my_job_%j.out  # Standard output and error log
 
-SBATCH --error=my_job_%j.err  # Error file. '%j' is replaced with the job ID
+#SBATCH --error=my_job_%j.err  # Error file. '%j' is replaced with the job ID
 
-SBATCH --ntasks=1  # Run on a single task
+#SBATCH --ntasks=1  # Run on a single task
 
-SBATCH --cpus-per-task=4  # Number of CPU cores per task
+#SBATCH --cpus-per-task=32  # Number of CPU cores per task
 
-SBATCH --time=23:00:00  # Time limit hrs:min:sec
+#SBATCH --time=23:00:00  # Time limit hrs:min:sec
 
-SBATCH --partition=standard  # Specify the partition to submit to
+#SBATCH --partition=standard  # Specify the partition to submit to
 
  
 time python train_insert_monotonic.py --train robustness_spec/seed_train_malicious/mutate_insert_any_pt1/pickles --model_name robust_combine_two
